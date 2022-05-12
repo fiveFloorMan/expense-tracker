188d96bdcfeea7f6593faa2422a53fc6d31444c8
fiveFloorMan <harvieusegithub@gmail.com>
2022-05-12 17:56:18 +0800

    add login feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -11,8 +11,13 @@ router.get('/login', (req, res) => {
 // login 介面回傳data
 router.post('/login', (req, res) => {
   const { name, password } = req.body
-  console.log(`name: ${name}; password: ${password}` )
-  res.redirect('/')
+  User.findOne({ name, password })
+    .lean()
+    .then(user => {
+      console.log(`welcome back ${name} !`)
+    })
+    .catch(error => console.log(error))
+  res.render('index', {name})
 })
 
 // register 介面
@@ -22,12 +27,15 @@ router.get('/register', (req, res) => {
 // register 介面回傳data
 router.post('/register', (req, res) => {
   const {name, password, confirmPassword} = req.body
+  // 辨識password 和 confirmPassword 是否一致
   if (password !== confirmPassword){
     return res.render('register', { name, password })
   }
+  // 成立新帳號
   User.create({ name, password })
     .then()
     .catch(error => console.log(error))
   res.redirect('/users/login')
 })
+
 module.exports = router
