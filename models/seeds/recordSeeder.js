44f44aabe25a9bbd4cd7bd6e3017acebe9f4d246
fiveFloorMan <harvieusegithub@gmail.com>
2022-05-12 21:24:32 +0800

    add categoryList

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,13 +1,34 @@
+// 暫時放棄中 目前的狀態是發現User.create 到 User.findOne之間短
+
 const db = require('../../config/mongoose')
 
+const User = require('../user')
 const Record = require('../record')
 
 db.once('open', () => {
-  Record.create({
-    name: 'testRecord',
-    date: Date.now(),
-    amount: 100,
+  
+  User.create({
+    name: 'test',
+    password: 'test'
   })
+  
+  User.findOne({ name : 'test' })
+    .lean()
+    .then(user => {
+      console.log('user: ', user)
+      const userId = user._id
+      
+      Record.create({
+        name: 'Movie(test)',
+        date: Date.now(),
+        amount: 250,
+        userId: userId,
+      })
+      
+    })
+    
+
+    
     .then(() => console.log('recordSeeder.js is done'))
     .then(() => process.exit())
 })
