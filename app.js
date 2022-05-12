1b7b7560a0bed9d3cc79972195fca159427bcdf0
fiveFloorMan <harvieusegithub@gmail.com>
2022-05-12 14:17:47 +0800

    Setting express-handlebars

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 
 const app = express()
 const PORT = 3000
 
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
+
 require('./config/mongoose')
 
 app.get('/', (req, res) => {
-  res.send('app.js is working')
+  res.render('index')
 })
 
 app.listen(PORT, () => {
