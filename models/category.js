a60fd1302a861ab52aca0814dc7324704f08b715
fiveFloorMan <harvieusegithub@gmail.com>
2022-05-12 22:28:56 +0800

    Run seeder of category

--- models/category.js
+++ models/category.js
@@ -6,6 +6,10 @@ const categorySchema = new Schema({
     type: String,
     required: true,
   },
+  icon: {
+    type: String,
+    required: true
+  }
 })
 
 module.exports = mongoose.model('Category', categorySchema)
--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -1,9 +1,17 @@
 const Category = require('../category')
 
 const db = require('../../config/mongoose')
+const categoryList = require('./categoryList.json')
 
 db.once('open', () => {
-  Category.create({ name: 'testCategory' })
+
+  return Promise.all(Array.from({ length: 5 }, (_, index) => {
+    return Category.create({ 
+      name: categoryList[index].name,
+      icon: categoryList[index].icon
+    })
+  }))
+
     .then(() => console.log('categorySeeder.js is done'))
     .then(() => process.exit())
 })
