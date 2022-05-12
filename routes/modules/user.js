38a8af02c3a5d7dd1a1da5ca724d92507435041a
fiveFloorMan <harvieusegithub@gmail.com>
2022-05-12 17:10:35 +0800

    register data is done

--- routes/modules/user.js
+++ routes/modules/user.js
@@ -2,15 +2,23 @@
 const express = require('express')
 const router = express.Router()
 
+// login 介面
 router.get('/login', (req, res) => {
   res.render('login')
 })
+// login 介面回傳data
 router.post('/login', (req, res) => {
   res.redirect('/')
 })
 
+// register 介面
 router.get('/register', (req, res) => {
   res.render('register')
 })
-
+// register 介面回傳data
+router.post('/register', (req, res) => {
+  const {name, password} = req.body
+  console.log(`name: ${name} ; password: ${password}`)
+  res.redirect('/')
+})
 module.exports = router
