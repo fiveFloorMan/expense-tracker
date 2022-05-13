bd4b8d3d5b9b9860bda094c623f20595edc67c9e
fiveFloorMan <harvieusegithub@gmail.com>
2022-05-13 15:46:17 +0800

    add seeder of category

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -6,10 +6,11 @@ const categoryList = require('./categoryList.json')
 db.once('open', () => {
 
   return Promise.all(Array.from({ length: 5 }, (_, index) => {
-    return Category.create({ 
-      name: categoryList[index].name,
-      icon: categoryList[index].icon
-    })
+      console.log(`A`)
+      return Category.create({ 
+        name: categoryList[index].name,
+        icon: categoryList[index].icon
+      })
   }))
 
     .then(() => console.log('categorySeeder.js is done'))
