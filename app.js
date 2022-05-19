e19976ef0d59a0f073b81e1a6a4b2c7bc3d21790
fiveFloorMan <harvieusegithub@gmail.com>
2022-05-19 17:49:49 +0800

    add express-session

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const routes = require('./routes')
 const bodyParser = require('body-parser')
@@ -8,6 +9,12 @@ const PORT = 3000
 
 app.engine('handlebars', exphbs())
 app.set('view engine', 'handlebars')
+
+app.use(session({
+  secret: 'ExpenseTrackerSecret',
+  resave: false,
+  saveUninitialized: true
+}))
 
 require('./config/mongoose')
 app.use(bodyParser.urlencoded({ extended: true }))
