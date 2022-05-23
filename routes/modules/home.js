7491134df80d49ad179c643114eac54071ea6dcb
fiveFloorMan <harvieusegithub@gmail.com>
2022-05-23 21:13:59 +0800

    add index option

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -5,30 +5,94 @@ const router = express.Router()
 const Record = require('../../models/record')
 const Category = require('../../models/category')
 
-// 符合使用者的個人紀錄
+// 符合使用者的個人紀錄(無選擇類別情況)
 router.get('/', (req, res) => {
   const userId = res.locals.user
   Record.find({ userId })
     .populate('categoryId', Category)
     .lean()
     .then(records => { 
+      if(req.body.categorySelectName){
+        console.log('req.body.categorySelectName:', req.body.categorySelectName)
+      }
       let totalAmount = 0
+      let categoryArray = []
       Promise.all(Array.from(records, record => {
+        // 計算總花費
         totalAmount += record.amount 
+        // 選出簡短的日期, 表示方式: yyyy-mm-dd
         let createYear = record.date.getYear() + 1900
         createYear = createYear.toString()
         let createMonth = record.date.getMonth() + 1
         createMonth = createMonth.toString()
         let createDate = record.date.getDate().toString()
-        // console.log('createMonth:',createMonth)
-        // console.log('createDate:',createDate)
-
         record.date = createYear + "-" + createMonth + "-" + createDate
+        // 挑出所有的類別
+        categoryArray.push(record.categoryId.name)
       }))
-      return res.render('index', { records, totalAmount }) 
+      // 刪除重複推入的類別
+      const categoryList = categoryArray.filter((element, index, self) => {
+        return self.indexOf(element) === index
+      })
+      return res.render('index', { records, totalAmount, categoryList }) 
     })
     .catch(error => console.log(error))
 })
+
+// 符合使用者的個人紀錄(有選擇類別情況)
+router.post('/', (req, res) => {
+  const categorySelectName = req.body.categorySelectName
+  const userId = res.locals.user
+  Record.find({ userId })
+    .populate('categoryId', Category)
+    .lean()
+    .then(records => {
+      let totalAmount = 0
+      let categoryArray = []
+      let selectRecords = []
+      Promise.all(Array.from(records, record => {
+        // 挑出所有的類別
+        categoryArray.push(record.categoryId.name)
+        // 以下是符合所選類別的record計算
+        if (record.categoryId.name === categorySelectName) {
+          console.log('record.categoryId.name:', record.categoryId.name)
+          // 計算總花費
+          totalAmount += record.amount 
+          // 選出簡短的日期, 表示方式: yyyy-mm-dd
+          let createYear = record.date.getYear() + 1900
+          createYear = createYear.toString()
+          let createMonth = record.date.getMonth() + 1
+          createMonth = createMonth.toString()
+          let createDate = record.date.getDate().toString()
+          record.date = createYear + "-" + createMonth + "-" + createDate
+          // 符合條件的records
+          return selectRecords.push(record)
+        } 
+        if (categorySelectName === "全部") {
+          console.log('record.categoryId.name:', record.categoryId.name)
+          // 選擇全部的record計算
+          // 計算總花費
+          totalAmount += record.amount 
+          // 選出簡短的日期, 表示方式: yyyy-mm-dd
+          let createYear = record.date.getYear() + 1900
+          createYear = createYear.toString()
+          let createMonth = record.date.getMonth() + 1
+          createMonth = createMonth.toString()
+          let createDate = record.date.getDate().toString()
+          record.date = createYear + "-" + createMonth + "-" + createDate
+          // 全部的records
+          return selectRecords.push(record)
+        }
+      }))
+      console.log('selectRecords:', selectRecords)
+      const categoryList = categoryArray.filter((element, index, self) => {
+        return self.indexOf(element) === index
+      })
+      return res.render('index', { totalAmount, categoryList, selectRecords}) 
+    })
+    .catch(error => console.log(error))
+})
+
 
 module.exports = router
 
